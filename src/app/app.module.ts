f25434795a5db6e568b7c0ecf8780c59d5e459bb
tbone7243 <tbone7243@gmail.com>
2020-08-12 17:41:42 -0400

    Added empty shell templates of the components to make the app structure.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { CreateComponent } from './components/create/create.component';
+import { SearchComponent } from './components/search/search.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegistrationComponent,
+    ProfileComponent,
+    CreateComponent,
+    SearchComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
