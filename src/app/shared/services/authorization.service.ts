88b7c098d0420e64b427574bc5a168de6ed7e0fd
tbone7243 <tbone7243@gmail.com>
2020-08-14 10:10:09 -0400

    Adjusted the services, renamed posts to request and created a listing service.

--- src/app/shared/services/authorization.service.ts
+++ src/app/shared/services/authorization.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { User } from '../models/user.model';
+import { Organization } from '../models/organization.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorizationService {
 
-  constructor() { }
+  orgApi: string = 'http://localhost:3000/orgs';
+  userApi: string = 'http://localhost:3000/users';
+
+  public organizations: Organization[];
+  public users: User[];
+
+  constructor(
+    private http: HttpClient,
+    public router: Router
+  ) { }
+//Register a user
+registerUser() {
+
+}
+login() {
+
+}
+getProfile() {
+
+}
+
+
 }
