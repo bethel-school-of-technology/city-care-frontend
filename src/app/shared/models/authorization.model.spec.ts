01307b0925d08e29579c0104f33766f2e9f44356
tbone7243 <tbone7243@gmail.com>
2020-08-23 14:59:40 -0400

    updated.

--- src/app/shared/models/authorization.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Authorization } from './authorization.model';
-
-describe('Authorization', () => {
-  it('should create an instance', () => {
-    expect(new Authorization()).toBeTruthy();
-  });
-});
--- src/app/shared/models/authorization.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Authorization {
-          email: string;
-          password: string;
-          role: boolean;
-          active: boolean;
-}
